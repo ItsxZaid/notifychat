0a2790b6ea673d6e31c4675fe9eaff2d61be2a8b
ItsxZaid <zaidzeenathe@gmail.com>
2025-11-04 01:38:43 +0530

    Work in progress :)

--- internal/store/campaign_store.go
+++ internal/store/campaign_store.go
@@ -3,20 +3,20 @@ package store
 import (
 	"context"
 	"solution-for-x/notifychat/internal/domain"
-	"solution-for-x/notifychat/internal/store/db.go"
+	"solution-for-x/notifychat/internal/store/sqlc_generated"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type CampaignStore struct {
-	db *db.Queries
+	db *sqlc_generated.Queries
 }
 
 var _ domain.CampaignRepository = (*CampaignStore)(nil)
 
 func NewCampaignStore(pool *pgxpool.Pool) *CampaignStore {
 	return &CampaignStore{
-		db: db.New(pool),
+		db: sqlc_generated.New(pool),
 	}
 }
 
